59ae43b8cbb2b683d969d7382a07f3173a6a5e60
daunikman <daunikman@gmail.com>
2020-07-19 05:28:38 +0100

    File Structure: Added gitIgnore file. Code Review: Added Comments in the code base

--- src/com/account/Account.java
+++ src/com/account/Account.java
@@ -21,6 +21,13 @@ public class Account {
         return pin;
     }
 
+
+    /*todo  Hey Grace, what happens to IF , IFELSE and ELSE statement constructs?
+    I think it would be neater if i get one error message at a time, or don't you think so?
+    Maybe you can decide to throw the first error (wrong pin) and when the person finally gets it right then you can now access his account and tell him he has no funds.
+    How would you feel when thieves attempt to use your ATM and they are told you are a broke girl and attempting to hack your abeokuata account is a waste of time? i know you will feel good shan
+    but imagine if you truly have funds ? won't they take the effort to continue trying pins?
+    */
     public void withdrawMoney(int pin, int amount) {
         if (amount<=accountBalance-1000 && pin == this.pin) {
             accountBalance-=amount;
@@ -35,3 +42,4 @@ public class Account {
 
 
 }
+//@todo - Good work so far though, see you at the village ... Chibuzo Ekejiuba... Semicolon Africa
